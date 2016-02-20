d30843fe77309cddf8b8386b2990bf28c432c89f
lavi061997 <lavi061997@gmail.com>
2016-02-19 23:18:27 -0800

    Backend
    Almost done with making mvp

---
+++ index.php
@@ -0,0 +1,25 @@
+
+<?php
+
+    
+
+   $dbhost = 'localhost:3306';
+   $dbuser = 'root';
+   $dbpass = '9582929870';
+
+   $dbname = 'random';
+$conn= mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
+if (mysqli_connect_errno($conn))
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+mysqli_select_db($conn,'random') or die ("no database"); 
+$query=mysqli_query($conn,"SELECT url FROM ufc
+ORDER BY RAND()
+LIMIT 1");
+ 
+if ($result = mysqli_fetch_array($query))
+{
+echo '<a href="javascript: void(0)" onclick="window.open(' . '\'' . $result['url'] . '\'' . ');">YYAAY</a>';
+}
+?>
