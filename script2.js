89b289540e3a1cc24aa175ca7e7db3b6c0dd6388
Ayush Mandowara <ayushxx7@gmail.com>
2016-02-20 09:24:44 +0530

    syncing

--- script2.js
+++ script2.js
@@ -8,9 +8,3 @@ var randomColor = function() {
     var b = safeColors[rand()];
     return "#"+r+g+b;
 };
-function hover(img) {
-    img.setAttribute('src', 'http://dummyimage.com/100x100/eb00eb/fff');
-}
-function unhover(img) {
-    img.setAttribute('src', 'http://dummyimage.com/100x100/000/fff');
-}
